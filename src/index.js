86744060a6176f9bb5cf1169eed9d15ab61715cc
Bryce Hoover <bryhoover11@gmail.com>
2021-07-29 19:22:10 -0500

    Component added, refactor

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => {
-    return (
-        <div>
-            Hello, world!
-        </div> 
-    )};
+import App from './App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
