2f52e7e975a28e8eeec6816efb64d070484f7de0
Bryce Hoover <bryhoover11@gmail.com>
2021-07-30 00:32:10 -0500

    Image API display

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const ImageList = (props) => {
-    console.log(props.images);
+    const images = props.images.map(({ description, id, urls }) => <img alt={description} key={id} src={urls.regular}/>);
 
     return (
         <div>
-            ImageList
+            {images}
         </div>
     )
 }
