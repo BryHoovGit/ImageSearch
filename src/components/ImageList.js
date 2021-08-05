3e9d342665f57f51689398d1826c824111b8734f
Bryce Hoover <bryhoover11@gmail.com>
2021-08-05 10:15:42 -0500

    ImageCard component added, refactor

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import'../stylesheets/ImageList.css'
+import ImageCard from '../components/ImageCard';
 
 const ImageList = (props) => {
-    const images = props.images.map(({ description, id, urls }) => <img alt={description} key={id} src={urls.regular}/>);
+    const images = props.images.map(( image ) => { 
+        return <ImageCard key={image.id} image={image}/>;
+    });
 
     return (
         <div className="image-list">
